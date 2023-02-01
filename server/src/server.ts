d6399cf7e33ba4721617736e76579d104c525cba
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-01 17:30:31 +0300

    fix: configuration for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,24 @@
 import express from 'express';
+
 const app = express();
 const port = 5000;
 const urlencodedParser = express.urlencoded({extended: false});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// app.get('/reg', (req, res) => {
-//     res.send('registration');
+// app.get('/', (req, res) => {
+//     res.send(`main page`);
 // });
 
+app.get('/', (req, res) => {
+    res.send(`main page`);
+});
+
+app.get('/registration', (req, res) => {
+    res.send('    <h1>Введите данные</h1>\n    <form method="post">\n        <label>Имя</label><br>\n        <input type="text" name="userName" /><br><br>\n        <label>Возраст</label><br>\n        <input type="number" name="userAge" /><br><br>\n        <input type="submit" value="Отправить" />\n    </form>');
+});
+
 app.post("/registration", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    console.log(`${request.body.userName} - ${request.body.userAge}`);
     response.send(`${request.body.userName} - ${request.body.userAge}`);
 });
 
