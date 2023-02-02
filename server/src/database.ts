9a67dd12f2b627f4210b65635c9de0eae1b5a9f2
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-02 14:31:13 +0300

    fix: lint fixes

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,10 +1,10 @@
 import { Database } from 'sqlite3';
 
 // Open a SQLite database, stored in the file db.sqlite
-const db = new Database('../src/db/db.sqlite', (err) => err ? console.log(err) : 'connected');
+const db = new Database('db/db.sqlite', (err) => err ? console.log(err) : 'connected');
 // Fetch a random integer between -99 and +99
 // let res = db.get('SELECT * FROM Users', (err,res) => err ? console.log(err) : 'successfully add')
 console.log(db.all(
-    'SELECT * FROM Users',
-    (_, res) => console.log(res[0])
-))
+  'SELECT * FROM Users',
+  (_, res) => console.log(res[0]),
+));
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,30 @@
 import express from 'express';
+import './chat';
+import './database';
 
 const app = express();
 const port = 5000;
-const urlencodedParser = express.urlencoded({extended: false});
-
+const urlencodedParser = express.urlencoded({ extended: false });
 // app.get('/', (req, res) => {
 //     res.send(`main page`);
 // });
 
 app.get('/', (req, res) => {
-    res.send(`main page`);
+  res.send('main page');
 });
 
 app.get('/registration', (req, res) => {
-    res.send('    <h1>Введите данные</h1>\n    <form method="post">\n        <label>Имя</label><br>\n        <input type="text" name="userName" /><br><br>\n        <label>Возраст</label><br>\n        <input type="number" name="userAge" /><br><br>\n        <input type="submit" value="Отправить" />\n    </form>');
+  res.send('    <h1>Введите данные</h1>\n    <form method="post">\n        <label>Имя</label><br>\n        <input type="text" name="userName" /><br><br>\n        <label>Возраст</label><br>\n        <input type="number" name="userAge" /><br><br>\n        <input type="submit" value="Отправить" />\n    </form>');
 });
 
-app.post("/registration", urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400);
-    console.log(`${request.body.userName} - ${request.body.userAge}`);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
+app.post('/registration', urlencodedParser, function (request, response) {
+  if (!request.body) return response.sendStatus(400);
+  console.log(`${request.body.userName} - ${request.body.userAge}`);
+  response.send(`${request.body.userName} - ${request.body.userAge}`);
 });
-
-app.listen(port, () => {
+if (require.main === module) {
+  app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
-});
+  });
+}
+export default app;
