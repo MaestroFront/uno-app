4ba059986a1c158d5934a725fc3eb7880e401d0a
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-02 14:27:48 +0300

    feat: add lint webpack config

---
+++ server/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+
+module.exports = {
+    entry: './src/server.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    target: 'node',
+    watch: true,
+    externals: [ nodeExternals() ],
+    plugins: [
+        new WebpackShellPluginNext({
+            onBuildStart:{
+                scripts: ['echo "===> Starting packing with WEBPACK 5"'],
+                blocking: true,
+                parallel: false
+            },
+            onBuildEnd:{
+                scripts: ['npm run run:server'],
+                blocking: false,
+                parallel: true
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
