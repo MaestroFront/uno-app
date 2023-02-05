76f3da1211e62db6e0b0a64910aa291701804447
Maestro <akievaleksandr@mail.ru>
2023-02-05 12:31:59 +0500

    refactor: update index.ts

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,13 @@
 import './style.scss';
 import Controller from './controller';
+
 import './components/cards/cards';
-import './components/main-page/main-page';
 import './components/rules-page/rules-page';
 import './components/header/header';
 
-console.log('hello');
-Controller.start();
-//alert('fsfds');
-console.log(
-  'dfffffffffffffffffffffff fsdfffffffffffffffffffffffffffffsdf sdfdsfdfs fsssssssssssssssssssfsfs sfdfsdfsdfdsf',
-);
+import { createPage } from './components/helpers/helpers';
+
+window.onload = () => {
+  createPage();
+  Controller.start();
+};
