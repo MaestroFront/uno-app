c56fcef39e445af6e0668eb21ebae47036d3f038
Maestro <akievaleksandr@mail.ru>
2023-02-05 11:44:24 +0500

    refactor: update main file

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,17 @@
 import './style.scss';
 import Controller from './controller';
 import './main-page/main-page';
+import { createElement } from './helpers/helpers';
+import { createHeader, createMainPage } from './main-page/main-page';
+import { createDevelopedByContainer } from './developed-by/developed-by';
+
+const body = document.querySelector('.body') as HTMLDivElement;
+const header = createElement('header', 'header') as HTMLDivElement;
+const main = createElement('main', 'main') as HTMLDivElement;
+const footer = createElement('footer', 'footer') as HTMLDivElement;
+body.append(header, main, footer, createDevelopedByContainer());
+createHeader();
+createMainPage();
 
 console.log('hello');
 Controller.start();
