282d2e125d4d7866ce5e1e6413353d1bc008d773
Maestro <akievaleksandr@mail.ru>
2023-02-05 11:41:56 +0500

    feat: add game field page functional

---
+++ app/src/game-field/game-field.ts
@@ -0,0 +1,63 @@
+import {
+  createElement,
+  createImage,
+  createParagraph,
+} from '../helpers/helpers';
+
+const playerField = (playerClassName: string, playerName: string) => {
+  const block = createElement('div', playerClassName) as HTMLDivElement;
+  const cardsBlock = createElement('div', 'cards');
+  const title = createParagraph('player-name', playerName);
+  block.append(cardsBlock, title);
+
+  return block;
+};
+
+const createRhomb = () => {
+  const xmlns = 'http://www.w3.org/2000/svg';
+  const rhomb = document.createElementNS(xmlns, 'svg');
+  rhomb.classList.add('rhomb');
+  rhomb.setAttributeNS(null, 'viewBox', '0 0 512 512');
+  const path = document.createElementNS(xmlns, 'path');
+  path.setAttribute('fill-rule', 'evenodd');
+  path.setAttribute(
+    'd',
+    'M113.289264,70.6225973 L368.951995,70.6225973 C392.516145,70.6225973 411.618662,89.7251147 411.618662,113.289264 L411.618662,368.951995 C411.618662,392.516145 392.516145,411.618662 368.951995,411.618662 L113.289264,411.618662 C89.7251147,411.618662 70.6225973,392.516145 70.6225973,368.951995 L70.6225973,113.289264 C70.6225973,89.7251147 89.7251147,70.6225973 113.289264,70.6225973 Z',
+  );
+  path.setAttribute('transform', 'rotate(45 230.6 266.521)');
+  rhomb.append(path);
+
+  return rhomb;
+};
+
+export const createGameField = (quantity: number) => {
+  const main = document.querySelector('.main') as HTMLDivElement;
+  const container = createElement('div', 'game-field') as HTMLDivElement;
+  if (quantity === 2)
+    container.append(
+      playerField('player-1', 'player 1'),
+      playerField('player-2', 'player 2'),
+    );
+  if (quantity === 3)
+    container.append(
+      playerField('player-1', 'player 1'),
+      playerField('player-2', 'player 2'),
+      playerField('player-3', 'player 3'),
+    );
+  if (quantity === 4)
+    container.append(
+      playerField('player-1', 'player 1'),
+      playerField('player-2', 'player 2'),
+      playerField('player-3', 'player 3'),
+      playerField('player-4', 'player 4'),
+    );
+  const field = createElement('div', 'field');
+  const deck = createElement('div', 'deck');
+  const currentCard = createElement('div', 'current-card');
+  const uno = createImage('uno', '../assets/img/logo-UNO.png', 'uno');
+
+  field.append(deck, currentCard, createRhomb(), uno);
+  container.append(field);
+
+  main.append(container);
+};
