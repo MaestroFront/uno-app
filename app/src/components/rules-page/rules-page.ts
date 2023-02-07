3830fdcbd025701f012edf794b09833bfda6a1a6
Anneli Savchenko <foranneli@gmail.com>
2023-02-07 22:49:18 +0100

    refactor: refactor code for rules-page in main.ts, rules-page.ts

--- app/src/components/rules-page/rules-page.ts
+++ app/src/components/rules-page/rules-page.ts
@@ -2,41 +2,112 @@ import { blueColor, greenColor, redColor, yellowColor, renderCardWithNumber, ren
 import { langData } from '../data';
 import { addButtonBackToMainPage, createButton, createElement, createParagraph } from '../helpers/helpers';
 
+const flipCard = (e: Event): void => {
+  const element = e.target as HTMLButtonElement;
+  const parent = element.parentNode?.parentNode as HTMLDivElement;
+  // console.log(parent);
+  parent.classList.toggle('open');
+  parent.querySelector('.card-block-front')?.classList.toggle('open');
+  parent.querySelector('.card-block-back')?.classList.toggle('open');
+};
 
-const createCardBlock  = (currCard: Element, cardTitle: string, cardValue: string, cardText: string, classNameCard: string, classNameCardFront: string): HTMLDivElement => {
-  const container = createElement('div', 'card-container') as HTMLDivElement;
-
-  const card = createElement('div', 'card-block') as HTMLDivElement;
-  card.classList.add(classNameCard);
+const createCardFront = (currCard: Element): HTMLDivElement => { //cardTitle: string, 
   const cardFront = createElement('div', 'card-block-front') as HTMLDivElement;
-  const btnFlip = createButton('btn-flip', 'button', 'flip');
-  cardFront.append(btnFlip);
-  cardFront.classList.add(classNameCardFront);
-
   const cardImgWrapper = createElement('div', 'card-img-wrapper') as HTMLDivElement;
+  cardImgWrapper.append(currCard);
+  // cardFront.classList.add(classNameCardFront);
+
   const cardDescription = createElement('div', 'card-description') as HTMLDivElement;
-  const showBtn = createButton('btn-show', 'button', 'show');
   
+  
+  //const title = createParagraph('card-title', cardTitle);
+  //
+  // 
+
+  //cardDescription.append(title, value);
+  
+  const btnReadMore = createButton('btn-read', 'button', 'read more...');
+  //cardImgWrapper.append(btnReadMore);
+  cardFront.append(cardImgWrapper, cardDescription, btnReadMore); 
+
+  btnReadMore.addEventListener('click', (e) => {
+    flipCard(e);
+  } );
+
+  return cardFront;
+};
+
+const createCardBack = (cardTitle: string, cardValue: string, cardText: string): HTMLDivElement => {
+  const cardBack = createElement('div', 'card-block-back') as HTMLDivElement;
+  // cardBack.classList.add(classNameCardBack);
+  
+  const btnFlip = createButton('btn-flip', 'button', 'flip card');
+  const cardDescription = createElement('div', 'card-description') as HTMLDivElement;
   const title = createParagraph('card-title', cardTitle);
   const value = createParagraph('card-value', cardValue);
   const text = createParagraph('card-text', cardText);
-  
-  cardImgWrapper.append(currCard);
+
   cardDescription.append(title, value, text);
-  card.append(cardFront, cardImgWrapper, cardDescription, showBtn);
-  container.append(card, cardFront);
+  // cardBack.append(btnFlip);
+
+  btnFlip.addEventListener('click', (e) => {
+    flipCard(e);
+  } );
+
+  cardBack.append(cardDescription, btnFlip);
+  return cardBack;
+};
+
+const createCardBlock  = (currCard: Element, cardTitle: string, cardValue: string, cardText: string): HTMLDivElement => {
+  const container = createElement('div', 'card-container') as HTMLDivElement;
+  container.append(createCardFront(currCard), createCardBack(cardTitle, cardValue, cardText));  //, cardTitle, 
+
   return container;
 };
+
+// const createBackCardBlock  = (currCard: Element, cardTitle: string, cardValue: string, cardText: string, classNameCard: string, classNameCardFront: string): HTMLDivElement => {
+//   const container = createElement('div', 'card-container') as HTMLDivElement;
+
+//   const card = createElement('div', 'card-block') as HTMLDivElement;
+//   card.classList.add(classNameCard);
+//   const cardFront = createElement('div', 'card-block-front') as HTMLDivElement;
+//   const btnFlip = createButton('btn-flip', 'button', 'flip');
+//   cardFront.append(btnFlip);
+//   cardFront.classList.add(classNameCardFront);
+
+//   const cardImgWrapper = createElement('div', 'card-img-wrapper') as HTMLDivElement;
+//   const cardDescription = createElement('div', 'card-description') as HTMLDivElement;
+//   const btnShow = createButton('btn-show', 'button', 'show');
+  
+//   const title = createParagraph('card-title', cardTitle);
+//   const value = createParagraph('card-value', cardValue);
+//   const text = createParagraph('card-text', cardText);
+  
+//   cardImgWrapper.append(currCard);
+//   cardDescription.append(title, value, text);
+//   card.append(cardFront, cardImgWrapper, cardDescription, btnShow);
+//   container.append(card, cardFront);
+
+//   btnFlip.addEventListener('click', (e) => {
+//     flipCard(e);
+//   } );
+
+//   btnShow.addEventListener('click', (e) => {
+//     flipCard(e);
+//   } );
+
+//   return container;
+// };
 
 const createCardsDescription = (): HTMLDivElement => {
   const cardsDescription = createElement('div', 'cards-description') as HTMLDivElement;
   
-  const numberedCard = createCardBlock(renderCardWithNumber('8', greenColor, 0.3), langData.ru['numbered-card-title'], langData.ru['numbered-card-points'], langData.ru['numbered-card-descr'], 'numbered-card', 'numbered-card-front');
-  const plusTwoCard = createCardBlock(renderPlusTwoCard(blueColor, 0.3), langData.ru['plustwo-card-title'], langData.ru['action-card-points'], langData.ru['plustwo-card-descr'], 'plustwo-card', 'plustwo-card-front');
-  const reverseCard = createCardBlock(renderReverseCard(redColor, 0.3), langData.ru['reverse-card-title'], langData.ru['action-card-points'], langData.ru['reverse-card-descr'], 'reverse', 'reverse-front');
-  const blockedCard = createCardBlock(renderBlockedCard(yellowColor, 0.3), langData.ru['blocked-card-title'], langData.ru['action-card-points'], langData.ru['blocked-card-descr'], 'blocked', 'blocked-front');
-  const plusFourCard = createCardBlock(renderPlusFourCard(0.3), langData.ru['plusfour-card-title'], langData.ru['black-card-points'], langData.ru['plusfour-card-descr'], 'plusfour-card', 'plusfour-card-front');
-  const multiCard = createCardBlock(renderMultiCard(0.3), langData.ru['multi-card-title'], langData.ru['black-card-points'], langData.ru['multi-card-descr'], 'multi-card', 'multi-card-front');
+  const numberedCard = createCardBlock(renderCardWithNumber('8', greenColor, 0.8), langData.ru['numbered-card-title'], langData.ru['numbered-card-points'], langData.ru['numbered-card-descr']);
+  const plusTwoCard = createCardBlock(renderPlusTwoCard(blueColor, 0.8), langData.ru['plustwo-card-title'], langData.ru['action-card-points'], langData.ru['plustwo-card-descr']);
+  const reverseCard = createCardBlock(renderReverseCard(redColor, 0.8), langData.ru['reverse-card-title'], langData.ru['action-card-points'], langData.ru['reverse-card-descr']);
+  const blockedCard = createCardBlock(renderBlockedCard(yellowColor, 0.8), langData.ru['blocked-card-title'], langData.ru['action-card-points'], langData.ru['blocked-card-descr']);
+  const plusFourCard = createCardBlock(renderPlusFourCard(0.8), langData.ru['plusfour-card-title'], langData.ru['black-card-points'], langData.ru['plusfour-card-descr']);
+  const multiCard = createCardBlock(renderMultiCard(0.8), langData.ru['multi-card-title'], langData.ru['black-card-points'], langData.ru['multi-card-descr']);
   cardsDescription.append(numberedCard, blockedCard, reverseCard, plusTwoCard, multiCard, plusFourCard);
 
   return cardsDescription;
@@ -70,18 +141,26 @@ export const createRulesPage = () => {
   main.append(createRulesBlock());
 };
 
-document.addEventListener('click', (e) => {
+// document.addEventListener('click', (e) => {
+//   const main = document.querySelector('.main') as HTMLDivElement;
+//   const element = e.target as HTMLButtonElement;
+//   if (element.closest('.btn-rules')) {
+//     main.innerHTML = '';
+//     addButtonBackToMainPage();
+//     createRulesPage();
+//   }
+//   // if (element.closest('.btn-show') || element.closest('.btn-flip')) {
+//   //   const parent = element.parentNode?.parentNode as HTMLDivElement;
+//   //   parent.classList.toggle('open');
+//   //   parent.querySelector('.card-block-front')?.classList.toggle('open');
+//   //   parent.querySelector('.card-block')?.classList.toggle('open');
+//   // }
+// });
+
+export const openRulesPage = () => {
   const main = document.querySelector('.main') as HTMLDivElement;
-  const element = e.target as HTMLButtonElement;
-  if (element.closest('.btn-rules')) {
-    main.innerHTML = '';
-    addButtonBackToMainPage();
-    createRulesPage();
-  }
-  if (element.closest('.btn-show') || element.closest('.btn-flip')) {
-    const parent = element.parentNode?.parentNode as HTMLDivElement;
-    parent.classList.toggle('open');
-    parent.querySelector('.card-block-front')?.classList.toggle('open');
-    parent.querySelector('.card-block')?.classList.toggle('open');
-  }
-});
+  main.innerHTML = '';
+  addButtonBackToMainPage();
+  createRulesPage();
+};
+
