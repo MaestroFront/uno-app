1db5207408266aa9120d570c09d9a2888b92948f
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-08 19:45:50 +0300

    refactor: replace controller code and add some id for elements

--- app/src/components/game-field/game-field.ts
+++ app/src/components/game-field/game-field.ts
@@ -3,12 +3,13 @@ import {
   createImage,
   createParagraph,
 } from '../helpers/helpers';
-import Controller, { CardInfo, WebSocketMessage } from '../../controller';
+import Controller from '../../controller';
 
 const playerField = (playerClassName: string, playerName: string) => {
   const block = createElement('div', playerClassName) as HTMLDivElement;
   const cardsBlock = createElement('div', 'cards');
   const title = createParagraph('player-name', playerName);
+  title.id = `name-${playerClassName}`;
   block.append(cardsBlock, title);
 
   return block;
@@ -31,7 +32,7 @@ const createRhomb = () => {
   return rhomb;
 };
 
-export const createGameField = async (quantity: number) => {
+export const createGameField = (quantity: number) => {
   const main = document.querySelector('.main') as HTMLDivElement;
   const container = createElement('div', 'game-field') as HTMLDivElement;
   if (quantity === 2) {
@@ -39,7 +40,6 @@ export const createGameField = async (quantity: number) => {
       playerField('player-1', 'player 1'),
       playerField('player-2', 'player 2'),
     );
-    Controller.createNewGameWithComputer(2);
   }
   if (quantity === 3) {
     container.append(
@@ -47,7 +47,6 @@ export const createGameField = async (quantity: number) => {
       playerField('player-2', 'player 2'),
       playerField('player-3', 'player 3'),
     );
-    Controller.createNewGameWithComputer(3);
   }
   if (quantity === 4) {
     container.append(
@@ -56,7 +55,6 @@ export const createGameField = async (quantity: number) => {
       playerField('player-3', 'player 3'),
       playerField('player-4', 'player 4'),
     );
-    Controller.createNewGameWithComputer(4);
   }
   const field = createElement('div', 'field');
   const deck = createElement('div', 'deck');
@@ -70,49 +68,5 @@ export const createGameField = async (quantity: number) => {
   /* мои подключени */
   deck.addEventListener('click', () => {
     Controller.webSocket.send(JSON.stringify({ action: 'GET_CARD_BY_USER', data: '' }));
-  });
-  function createSimpleCard(id: number, color: string, value: number) {
-    const div = createElement('div', 'simple-card');
-    div.style.width = '25px';
-    div.style.height = '50px';
-    div.style.backgroundColor = color;
-    div.style.color = 'white';
-    div.innerText = value.toString();
-    div.id = id.toString();
-    div.addEventListener('click', evt => {
-      const user: string = ((evt.target as HTMLDivElement).parentElement as HTMLElement).className;
-      const dataForSent = JSON.stringify({ userName: user, cardId: (evt.target as HTMLDivElement).id });
-      Controller.webSocket.send(JSON.stringify({ action: 'MOVE_BY_USER', data: dataForSent }));
-    });
-    return div;
-  }
-
-  Controller.webSocket.addEventListener('message', (message: MessageEvent<string>) => {
-    const msg: WebSocketMessage = JSON.parse(message.data) as WebSocketMessage;
-    switch (msg.action) {
-      case 'GET_CARD': {
-        const data: { player: string, card: CardInfo } = JSON.parse(msg.data) as { player: string, card: CardInfo };
-        console.log(data.player);
-        (document.querySelector(`.${data.player}`) as HTMLElement).append(createSimpleCard(data.card.id, data.card.color, data.card.value));
-        break;
-      }
-      case 'MESSAGE': {
-        // eslint-disable-next-line no-alert
-        alert(msg.data);
-        break;
-      }
-      case 'MOVE': {
-        const dataMove: { topCard: CardInfo, currentColor: string } = JSON.parse(msg.data) as { topCard: CardInfo, currentColor: string };
-        (document.querySelector('.current-card') as HTMLElement).innerHTML = '';
-        (document.getElementById(`${dataMove.topCard.id}`) as HTMLElement).remove();
-        (document.querySelector('.current-card') as HTMLElement).append(createSimpleCard(dataMove.topCard.id, dataMove.topCard.color, dataMove.topCard.value));
-        (document.querySelector('.rhomb') as SVGElement).style.fill = dataMove.currentColor;
-        break;
-      }
-      case 'UPDATE_CARD': {
-        (document.querySelector(`.${msg.data}`) as HTMLElement).innerHTML = '';
-        break;
-      }
-    }
   });
 };
