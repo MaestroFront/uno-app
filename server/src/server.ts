071f1403c7947d0db4887bab5b8299d867528a9c
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-14 23:56:19 +0300

    fix: starting server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,46 +1,16 @@
-// import express from 'express';
-// // import './chat';
-// // import './database';
-// //
-// const app = express();
-// const port = 9001;
-// const urlencodedParser = express.urlencoded({ extended: false });
-// app.get('/', (req, res) => {
-//     res.send(`main page`);
-// });
-//
-// app.get('/', (req, res) => {
-//   res.send('main page');
-// });
-//
-// app.get('/registration', (req, res) => {
-//   res.send('    <h1>Введите данные</h1>\n    <form method="post">\n        <label>Имя</label><br>\n        <input type="text" name="userName" /><br><br>\n        <label>Возраст</label><br>\n        <input type="number" name="userAge" /><br><br>\n        <input type="submit" value="Отправить" />\n    </form>');
-// });
-//
-// app.post('/registration', urlencodedParser, function (request, response) {
-//   if (!request.body) return response.sendStatus(400);
-//   console.log(`${request.body.userName} - ${request.body.userAge}`);
-//   response.send(`${request.body.userName} - ${request.body.userAge}`);
-// });
-// if (require.main === module) {
-//   app.listen(port, () => {
-//     return console.log(`Express is listening at http://localhost:${port}`);
-//   });
-// }
-// export default app;
-// import UnoGame from './game/uno-game';
-//
-// const game = new UnoGame();
-//
-// game.startGame().then(()=> console.log('Run!')).catch(err => console.log(err));
-// app.post('/game', urlencodedParser, function (request, response) {
-//   if (!request.body) return response.sendStatus(400);
-//   console.log(JSON.parse(request.body));
-//   response.send('ok');
-// });
-
-// import UnoGame from './game/uno-game';
-
 import WebsocketServer from './websocket';
+import { app } from './express';
+import DBUno from './database';
+import { DBUsers } from './game/types';
 const wss = new WebsocketServer(9001);
 wss.start();
+app.listen(9002, ()=>console.log('Сервер запущен...'));
+let res: DBUsers;
+// (async () => res = await DBUno.checkUsersExist('Kirill'))();
+// setTimeout(()=> console.log(res), 2000);
+// DBUno.checkUsersExist('Kirill').then(data => {
+//   if (typeof data !== 'undefined') {
+//     console.log('yes');
+//     console.log(data);
+//   }
+// });
