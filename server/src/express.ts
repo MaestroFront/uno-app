771179e4d6f043efe2c3e364130f84ce226ef923
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-15 17:10:38 +0300

    feat: registration and login on server side

--- server/src/express.ts
+++ server/src/express.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import chalk from 'chalk';
 import bodyParser from 'body-parser';
 import DBUno from './database';
+import { DBUsers, UserInfo } from './game/types';
+import { createHmac } from 'crypto';
 export const app = express();
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+function hashPassword(pass: string): string {
+  const secret = '666UNOgameGAMEuno999';
+  return createHmac('sha256', secret)
+    .update(pass)
+    .digest('hex');
+}
+
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -9,22 +22,40 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cookieParser('UNOsecretCOOKIE'), cors({ credentials: true, origin: 'http://localhost:9000' }));
 
-app.post('/registration', (req, res)=>{
-  console.log(req.body);
-  res.send((req.body));
+app.post('/registration', async (req, res)=>{
+  const user = req.body as UserInfo;
+  await DBUno.openDB('write').then(() => {
+    DBUno.db.get('SELECT * FROM Users where UserName = ?', [user.userName], (err, data: DBUsers) => {
+      if (data?.UserId !== undefined) {
+        console.log(chalk.yellow(`New user with nickname: '${user.userName}' try registered, but this nickname already exist...`));
+        res.send(JSON.stringify({ status: false }));
+      } else {
+        user.password = hashPassword(user.password);
+        DBUno.db.run('INSERT INTO Users(UserName, UserPassword, Email) VALUES(?, ?, ?)',
+          [user.userName, user.password, user.email],
+          (err) => {if (err) console.log(err);});
+        console.log(chalk.green(`New user with nickname: '${user.userName}' successful registered!`));
+        res.send(JSON.stringify({ status: true }));
+      }
+    });
+  }).then(()=> DBUno.closeDB()).catch();
 });
 
-app.post('/login', (req, res)=>{
-  // console.log(req.body);
-  // res.send((req.body));
-  DBUno.checkUsersExist(req.body.userName).then(data => {
-    if (typeof data !== 'undefined') {
-      console.log('yes');
-      console.log(data);
-    } else {
-      console.log('no');
-    }
-  });
+app.post('/login', async (req, res)=>{
+  const user = req.body as { userName: string, password: string };
+  await DBUno.openDB().then(()=> {
+    DBUno.db.get('SELECT * FROM Users where UserName = ?', [user.userName], (err, data: DBUsers) => {
+      if (data?.UserId !== undefined && data.UserPassword === hashPassword(user.password)) {
+        const d = new Date();
+        d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+        console.log(chalk.green(`A user with a nickname '${user.userName}' is logged into the site!`));
+        res.send(JSON.stringify({ status: true, data: `user=${user.userName};expires=${d.toString()}` }));
+      } else {
+        console.log(chalk.red(`Try user with a nickname '${user.userName}' logged`));
+        res.send(JSON.stringify({ status: false, data: '' }));
+      }
+    });
+  }).then(()=> DBUno.closeDB()).catch();
 });
