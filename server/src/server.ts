2fc01c6f9a08f9f12a829a7882f040d2c944a9d5
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-15 17:16:33 +0300

    refactor: delete comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,6 @@
 import WebsocketServer from './websocket';
 import { app } from './express';
-import DBUno from './database';
-import { DBUsers } from './game/types';
+import chalk from 'chalk';
 const wss = new WebsocketServer(9001);
 wss.start();
-app.listen(9002, ()=>console.log('Сервер запущен...'));
-let res: DBUsers;
-// (async () => res = await DBUno.checkUsersExist('Kirill'))();
-// setTimeout(()=> console.log(res), 2000);
-// DBUno.checkUsersExist('Kirill').then(data => {
-//   if (typeof data !== 'undefined') {
-//     console.log('yes');
-//     console.log(data);
-//   }
-// });
+app.listen(9002, ()=>console.log(chalk.bgCyan('Сервер запущен...')));
