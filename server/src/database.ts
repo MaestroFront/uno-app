0316f0deff75ad21e9fb2ef24fa6917225d75a65
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-15 17:08:50 +0300

    fix: work with database

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,73 +1,64 @@
-import { Database, OPEN_READONLY } from 'sqlite3';
+import { Database, OPEN_READONLY, OPEN_READWRITE } from 'sqlite3';
 // import path from 'path';
 import { DBUsers } from './game/types';
-
-// Open a SQLite database, stored in the file db.sqlite
+import chalk from 'chalk';
 
 class DBUno {
   static db: Database;
 
   static path = 'F:\\projects\\uno-app\\server\\src\\db\\db.sqlite';//path.join(__dirname, 'db/db.sqllite');
 
-  static readAllUsers(): void {
-    DBUno.db = new Database(DBUno.path, OPEN_READONLY, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Successfully connected to database in readonly mode!');
-        DBUno.db.all('SELECT * FROM Users',
-          (_, res: DBUsers[]) => console.log(...res),
-        );
-      }
-      DBUno.db.close((e) => {
-        if (e) {
-          console.log(e);
-        } else {
-          console.log('Successfully disconnected from database!');
-        }
-      });
+  static async readAllUsers(): Promise<void> {
+    await DBUno.openDB().then(() => {
+      DBUno.db.all('SELECT * FROM Users',
+        (_, res: DBUsers[]) => console.log(...res),
+      );
     });
   }
 
-  static async checkUsersExist(userName: string): Promise<DBUsers> {
-    DBUno.db = new Database(DBUno.path, OPEN_READONLY, (err) => {
+  static closeDB() {
+    DBUno.db.close((e) => {
+      if (e) {
+        console.log(e);
+      } else {
+        console.log(chalk.bgGreen('Successfully disconnected from database!'));
+      }
+    });
+  }
+
+  static async openDB(flag = 'read') {
+    DBUno.db = new Database(DBUno.path, flag === 'read' ? OPEN_READONLY : OPEN_READWRITE, (err) => {
       if (err) {
         console.log(err);
       } else {
-        console.log('Successfully connected to database in readonly mode!');
+        console.log(chalk.bgGreen(`Successfully connected to database in ${flag === 'read' ? 'readonly' : 'read/write'} mode!`));
       }
     });
-    return new Promise(function (resolve, reject) {
-      DBUno.db.get(`SELECT * FROM Users where UserName = '${userName}'`, function (err, row)  {
-        if (err) reject('Read error: ' + err.message);
-        else {
-          resolve(row as Promise<DBUsers>);
-        }
-      });
-    });
-    // return new Promise((resolve, reject) => {
-    //   let user: DBUsers;
-    //   DBUno.db = new Database(DBUno.path, OPEN_READONLY, (err) => {
-    //     if (err) {
-    //       console.log(err);
-    //     } else {
-    //       console.log('Successfully connected to database in readonly mode!');
-    //     }
-    //   });
-    //   DBUno.db.get(`SELECT * FROM Users where UserName = '${userName}'`,
-    //     (_, res: DBUsers) => {
-    //       user = res;
-    //     },
-    //   );
-    //   DBUno.db.close((err) => {
-    //     if (err) {
-    //       console.log(err);
-    //     } else {
-    //       console.log('Successfully disconnected from database!');
-    //     }
-    //   });
-    //   resolve(user);
-    // });
   }
+
+  // static async checkUsersExist(userName: string):void {
+  //   // await DBUno.openDB();
+  //   // console.log(DBUno.db.get('SELECT * FROM Users', () => {}).);
+  //   // return new Promise((resolve, reject) => {
+  //   //   DBUno.db.get('SELECT * FROM Users where UserName = ', function (err, row)  {
+  //   //     if (err) reject('Read error: ' + err.message);
+  //   //     else {
+  //   //       resolve(row as Promise<DBUsers>);
+  //   //     }
+  //   //   });
+  //   // });
+  // }
+
+  // static async addUserToDB(user: DBUsers): Promise<DBUsers> {
+  //   DBUno.openDB('write')
+  //   return new Promise(function (resolve, reject) {
+  //     DBUno.db.get(`SELECT * FROM Users where UserName = '${userName}'`, function (err, row)  {
+  //       if (err) reject('Read error: ' + err.message);
+  //       else {
+  //         resolve(row as Promise<DBUsers>);
+  //       }
+  //     });
+  //   });
+  // }
 }
 export default DBUno;
