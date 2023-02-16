51644dd1b92a521002f0e99a58e458257f66ed05
Anneli Savchenko <foranneli@gmail.com>
2023-02-16 12:58:31 +0100

    feat: add rules window to game field

--- app/src/components/game-field/game-field.ts
+++ app/src/components/game-field/game-field.ts
@@ -1,4 +1,5 @@
 import {
+  createButton,
   createElement,
   createImage,
   createParagraph,
@@ -8,6 +9,7 @@ import { renderBackSide, renderCardWithNumber, yellowColor } from '../cards/card
 import { getCardFromDeck } from './game-animation';
 import { renderChat } from '../chat/chat';
 import { renderDiamond, renderReverseMessage } from '../animated-items/animated-items';
+import { createRulesWindow, openRulesWindow } from '../rules-page/rules-page';
 
 const playerField = (playerClassName: string, playerName: string) => {
   const block = createElement('div', playerClassName) as HTMLDivElement;
@@ -163,7 +165,10 @@ export const createGameField = (quantity: number) => {
   field.append(deck, currentCard, createRhomb(), uno);
   container.append(field);
 
-  main.append(container, renderChat(), renderDiamond(), renderReverseMessage());
+  const btnRules = createButton('btn-small-rules', 'button', '?');
+  btnRules.addEventListener('click', openRulesWindow);
+
+  main.append(container, renderChat(), renderDiamond(), renderReverseMessage(), createRulesWindow(), btnRules);
   /* мои подключени */
   deck.addEventListener('click', (e) => {
     getCardFromDeck(e, 'bottom');//TODO..анимация карты в зависимости от позиции игрока: top, bottom, left, right
