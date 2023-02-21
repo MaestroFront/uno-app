9026789452129675e226a2234f16e4290dd64e4b
KirillDemyanenko <demyanenkokg@gmail.com>
2023-02-21 22:28:39 +0300

    fix: 404 page

--- app/src/components/error-page/error-page.ts
+++ app/src/components/error-page/error-page.ts
@@ -1,12 +1,16 @@
 import { createButton, createElement, createImage } from '../helpers/helpers';
+import Router from '../router';
 
 export const createErrorPage = () => {
   const body = document.querySelector('.body') as HTMLBodyElement;
+  body.innerHTML = '';
   const image = createImage('error-404', '../../assets/img/error.gif', 'error 404');
   const btnErrorContainer = createElement('div', 'error-btn-container') as HTMLDivElement;
   const button = createButton('btn-main-page', 'button', 'main page');
+  button.onclick = () => {
+    Router.setState('home');
+    Router.checkPage();
+  };
   btnErrorContainer.append(button);
-  body.innerHTML = '';
   body.append(image, btnErrorContainer);
-  console.log(window.location.href);
 };
