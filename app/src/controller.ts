25e7e1e0da192aabcae88a926c6825f820d13944
Maestro <akievaleksandr@mail.ru>
2023-03-01 02:55:51 +0500

    feat: add pointer events when not u turn

--- app/src/controller.ts
+++ app/src/controller.ts
@@ -4,7 +4,7 @@ import { createButton, createElement, createParagraph } from './components/helpe
 import { CardInfo, WebSocketMessage } from './types';
 import { blueColor, greenColor, redColor, renderBackSide, renderBlockedCard, renderCardWithNumber, renderMultiCard, renderPlusFourCard, renderPlusTwoCard, renderReverseCard, yellowColor } from './components/cards/cards';
 import { clickSoundPlay, getCardSoundPlay, getChooseSound } from './components/sounds';
-import { moveCurrCard } from './components/game-field/game-animation';
+import { mouseEventsStop, moveCurrCard } from './components/game-field/game-animation';
 import { chooseColorAnimation, showBlockAnimation, showRandomColor, showReverseAnimation } from './components/animated-items/animated-items';
 import Router from './components/router';
 import { moveCardToPlayers, renderDeck, renderOneCard } from './components/game-field/game-field';
@@ -191,6 +191,7 @@ class Controller {
                 }
                 default : {
                   (document.querySelector('#name-player-1') as HTMLDivElement).classList.add('current-player-move');
+                  mouseEventsStop(false);
                 }
               }
             }
